5fc0cdc0b9f83fcd41b9b30bbabac6d94c46a062
andreastkdf <andreaskostakis@gmail.com>
2019-04-24 20:26:26 +0100

    attempt to deploy on github pages

---
+++ build/precache-manifest.a0517372216d6fc390c6443e2f35b87a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3dd9bea66f874753c384",
+    "url": "/MyReads/static/css/main.0ad66255.chunk.css"
+  },
+  {
+    "revision": "3dd9bea66f874753c384",
+    "url": "/MyReads/static/js/main.3dd9bea6.chunk.js"
+  },
+  {
+    "revision": "37a71bc958a4446c7d27",
+    "url": "/MyReads/static/js/1.37a71bc9.chunk.js"
+  },
+  {
+    "revision": "970c0060ff174c587b9c",
+    "url": "/MyReads/static/js/runtime~main.970c0060.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/MyReads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/MyReads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/MyReads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "f35100b36a2ecb820520cc2e71ceeb17",
+    "url": "/MyReads/index.html"
+  }
+];
