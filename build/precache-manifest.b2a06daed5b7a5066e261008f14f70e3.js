88a630d0905cc50e88a0f90c7222f11ff41b47ba
andreastkdf <andreaskostakis@gmail.com>
2019-04-24 19:57:01 +0100

    deploy

---
+++ build/precache-manifest.b2a06daed5b7a5066e261008f14f70e3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1176f19214f6c2e89d10",
+    "url": "/MyReads/static/css/main.0ad66255.chunk.css"
+  },
+  {
+    "revision": "1176f19214f6c2e89d10",
+    "url": "/MyReads/static/js/main.1176f192.chunk.js"
+  },
+  {
+    "revision": "37a71bc958a4446c7d27",
+    "url": "/MyReads/static/js/1.37a71bc9.chunk.js"
+  },
+  {
+    "revision": "970c0060ff174c587b9c",
+    "url": "/MyReads/static/js/runtime~main.970c0060.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/MyReads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/MyReads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/MyReads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "ed230a1d059b0ac207b4a294be4096ae",
+    "url": "/MyReads/index.html"
+  }
+];
